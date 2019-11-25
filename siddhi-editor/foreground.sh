0a04919af44651860971ac86d250090fba95828c
Suhothayan <suhothayan@gmail.com>
2019-11-25 22:29:09 +0530

    Add foreground commands

--- siddhi-editor/foreground.sh
+++ siddhi-editor/foreground.sh
@@ -1,5 +1,9 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 echo "Download siddhi-tooling-5.1.1.zip"
 wget https://github.com/siddhi-io/distribution/releases/download/v5.1.1/siddhi-tooling-5.1.1.zip
+
 echo "Unzip siddhi-tooling-5.1.1.zip"
 unzip siddhi-tooling-5.1.1.zip
+
+echo "Start Siddhi Editor"
+./siddhi-tooling-5.1.1/bin/tooling.sh
