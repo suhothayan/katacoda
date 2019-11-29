eea30600db9b3bf763e1aac4bf9853a69e254bf2
Suhothayan <suhothayan@gmail.com>
2019-11-29 16:46:46 +0530

    update siddhi tooling

--- siddhi-editor/foreground.sh
+++ siddhi-editor/foreground.sh
@@ -1,6 +1,6 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 echo "Download siddhi-tooling-5.1.1.zip"
-wget https://github.com/siddhi-io/distribution/releases/download/v5.1.1/siddhi-tooling-5.1.1.zip
+wget https://github.com/suhothayan/distribution/releases/download/5.2.0/siddhi-tooling-5.2.0-SNAPSHOT.zip
 
 echo "Unzip siddhi-tooling-5.1.1.zip"
 unzip siddhi-tooling-5.1.1.zip
